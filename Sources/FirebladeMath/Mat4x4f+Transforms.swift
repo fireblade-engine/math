7857f3d050a40bf78d03a65b61b22ad4c397504c
Christian Treffs <ctreffs@gmail.com>
2018-02-10 21:57:06 +0100

    Fix rotation and translation function

--- Sources/FirebladeMath/Mat4x4f+Transforms.swift
+++ Sources/FirebladeMath/Mat4x4f+Transforms.swift
@@ -6,8 +6,9 @@
 //  Copyright © 2018 Fireblade. All rights reserved.
 //
 
-// SEE: https://github.com/g-truc/glm/blob/master/glm/gtc/matrix_transform.inl
+import simd
 
+// SEE: https://github.com/g-truc/glm/blob/master/glm/gtc/matrix_transform.inl
 public extension Mat4x4f {
 
 	/// Creates a perspective projection matrix
@@ -32,6 +33,25 @@ public extension Mat4x4f {
 		case .rightHanded:
 			return perspectiveProjectionRH(fovy: fovy, aspect: aspect, zNear: zNear, zFar: zFar)
 		}
+	}
+
+	static func rotation(radians: Float, axis: Vec3f) -> Mat4x4f {
+		let unitAxis: Vec3f = normalize(axis)
+		let ct: Float = cos(radians)
+		let st: Float = sin(radians)
+		let ci = 1 - ct
+		let x = unitAxis.x, y = unitAxis.y, z = unitAxis.z
+		return Mat4x4f.init(columns:(Vec4f(    ct + x * x * ci, y * x * ci + z * st, z * x * ci - y * st, 0),
+									 Vec4f(x * y * ci - z * st, ct + y * y * ci, z * y * ci + x * st, 0),
+									 Vec4f(x * z * ci + y * st, y * z * ci - x * st, ct + z * z * ci, 0),
+									 Vec4f(                  0, 0, 0, 1)))
+	}
+
+	static func translation(_ position: Vec3f) -> Mat4x4f {
+		return Mat4x4f.init(columns:(Vec4f(1, 0, 0, 0),
+									 Vec4f(0, 1, 0, 0),
+									 Vec4f(0, 0, 1, 0),
+									 Vec4f(position.x, position.y, position.z, 1)))
 	}
 }
 
@@ -81,28 +101,7 @@ private extension Mat4x4f {
 	}
 }
 
-import simd
-
-// Generic matrix math utility functions
-func matrix4x4_rotation(radians: Float, axis: float3) -> matrix_float4x4 {
-	let unitAxis = normalize(axis)
-	let ct = cosf(radians)
-	let st = sinf(radians)
-	let ci = 1 - ct
-	let x = unitAxis.x, y = unitAxis.y, z = unitAxis.z
-	return matrix_float4x4.init(columns:(vector_float4(    ct + x * x * ci, y * x * ci + z * st, z * x * ci - y * st, 0),
-										 vector_float4(x * y * ci - z * st, ct + y * y * ci, z * y * ci + x * st, 0),
-										 vector_float4(x * z * ci + y * st, y * z * ci - x * st, ct + z * z * ci, 0),
-										 vector_float4(                  0, 0, 0, 1)))
-}
-
-func matrix4x4_translation(_ translationX: Float, _ translationY: Float, _ translationZ: Float) -> matrix_float4x4 {
-	return matrix_float4x4.init(columns:(vector_float4(1, 0, 0, 0),
-										 vector_float4(0, 1, 0, 0),
-										 vector_float4(0, 0, 1, 0),
-										 vector_float4(translationX, translationY, translationZ, 1)))
-}
-
+// FIXME: --- refactor
 func matrix_perspective_right_hand(fovyRadians fovy: Float, aspectRatio: Float, nearZ: Float, farZ: Float) -> matrix_float4x4 {
 	let ys = 1 / tanf(fovy * 0.5)
 	let xs = ys / aspectRatio
