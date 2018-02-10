b8e046e147aa4d49bce1da1146d8ac9257b08fa7
Christian Treffs <ctreffs@gmail.com>
2018-02-10 21:22:56 +0100

    Fix infix operator matrix multiplication

--- Sources/FirebladeMath/Mat4x4f+Operators.swift
+++ Sources/FirebladeMath/Mat4x4f+Operators.swift
@@ -7,8 +7,6 @@
 
 import simd
 
-infix operator *
-
 public func * (lhs: Mat4x4f, rhs: Mat4x4f) -> Mat4x4f {
 	return matrix_multiply(lhs, rhs)
 }
--- Tests/FirebladeMathTests/FirebladeMathTests.swift
+++ Tests/FirebladeMathTests/FirebladeMathTests.swift
@@ -3,4 +3,10 @@ import XCTest
 
 class FirebladeMathTests: XCTestCase {
 
+	func testMat4x4fMultiplicationOperator() {
+
+		let resMat: Mat4x4f = Mat4x4f(diagonal: Vec4f(2)) * Mat4x4f(diagonal: Vec4f(3))
+
+		XCTAssert(resMat != Mat4x4f.init(diagonal: Vec4f(1)))
+	}
 }
