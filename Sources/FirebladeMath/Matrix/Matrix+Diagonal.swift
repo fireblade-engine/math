1478b1de2f03af6248c44f0f729dff0dc5254582
Christian Treffs <ctreffs@gmail.com>
2019-08-26 14:59:58 +0200

    Diagonal fix

--- Sources/FirebladeMath/Matrix/Matrix+Diagonal.swift
+++ Sources/FirebladeMath/Matrix/Matrix+Diagonal.swift
@@ -7,27 +7,36 @@
 
 #if !canImport(simd)
 extension Mat3x3d {
-    @inlinable public init(diagonal: Double) {
-        self.init(diagonal)
+    @inlinable public init(diagonal: Vec3d) {
+        self.init([diagonal.x, 0, 0],
+                  [0, diagonal.y, 0],
+                  [0, 0, diagonal.z])
     }
 }
 
 extension Mat3x3f {
-    @inlinable public init(diagonal: Float) {
-        self.init(diagonal)
+    @inlinable public init(diagonal: Vec3f) {
+        self.init([diagonal.x, 0, 0],
+                  [0, diagonal.y, 0],
+                  [0, 0, diagonal.z])
     }
 }
 
 extension Mat4x4d {
-    @inlinable public init(diagonal: Double) {
-        self.init(diagonal)
+    @inlinable public init(diagonal: Vec4d) {
+        self.init([diagonal.x, 0, 0, 0],
+                  [0, diagonal.y, 0, 0],
+                  [0, 0, diagonal.z, 0],
+                  [0, 0, 0, diagonal.w])
     }
 }
 
 extension Mat4x4f {
-    @inlinable public init(diagonal: Float) {
-        self.init(diagonal)
+    @inlinable public init(diagonal: Vec4f) {
+        self.init([diagonal.x, 0, 0, 0],
+                  [0, diagonal.y, 0, 0],
+                  [0, 0, diagonal.z, 0],
+                  [0, 0, 0, diagonal.w])
     }
 }
-
 #endif
