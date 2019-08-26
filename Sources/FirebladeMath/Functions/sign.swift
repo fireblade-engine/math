7486b8ce313e0be1714bf12e0a040253f72c2b05
Christian Treffs <ctreffs@gmail.com>
2019-08-26 13:53:58 +0200

    sign

--- Sources/FirebladeMath/Functions/sign.swift
+++ Sources/FirebladeMath/Functions/sign.swift
@@ -9,16 +9,6 @@
 import simd
 #endif
 
-#if canImport(SGLMath)
-// types
-import struct SGLMath.Vector2
-import struct SGLMath.Vector3
-import struct SGLMath.Vector4
-import protocol SGLMath.ArithmeticType
-
-import func SGLMath.sign
-#endif
-
 #if canImport(simd)
 public func sign(_ x: Double) -> Double {
     return simd_sign(x)
@@ -31,6 +21,5 @@ public func sign(_ x: Float) -> Float {
 
 /// Returns -1 if `x < 0`, +1 if `x > 0`, and 0 otherwise (`sign(NaN)` is 0).
 public func sign<S>(_ x: S) -> S where S: FloatingPointScalar {
-    // FIXME:SGLMath.sign(<#T##x: VectorType##VectorType#>)
-    fatalError("not implemented")
+    return x == 0 ? 0 : x < 0 ? -1 : 1
 }
