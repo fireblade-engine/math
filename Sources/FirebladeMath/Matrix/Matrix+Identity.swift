13628c0eb506058daa8e2d6f64a0fbcffd7ec2e9
Christian Treffs <ctreffs@gmail.com>
2019-08-27 10:35:41 +0200

    Identities

--- Sources/FirebladeMath/Matrix/Matrix+Identity.swift
+++ Sources/FirebladeMath/Matrix/Matrix+Identity.swift
@@ -6,17 +6,17 @@
 //
 
 extension Mat3x3f {
-    public static let identity = Mat3x3f(diagonal: .one)
+    public static let identity = Mat3x3f(diagonal: .init(1, 1, 1))
 }
 
 extension Mat3x3d {
-    public static let identity = Mat3x3d(diagonal: .one)
+    public static let identity = Mat3x3d(diagonal: .init(1, 1, 1))
 }
 
 extension Mat4x4f {
-    public static let identity = Mat4x4f(diagonal: .one)
+    public static let identity = Mat4x4f(diagonal: .init(1, 1, 1, 1))
 }
 
 extension Mat4x4d {
-    public static let identity = Mat4x4d(diagonal: .one)
+    public static let identity = Mat4x4d(diagonal: .init(1, 1, 1, 1))
 }
--- Sources/FirebladeMath/Vector/Vectors+One.swift
+++ Sources/FirebladeMath/Vector/Vectors+One.swift
@@ -7,41 +7,41 @@
 
 #if swift(<5.1)
 extension Vec2d {
-    public static let one = SIMD2<Double>(repeating: 1)
+    public static let one = Vec2d(repeating: 1)
 }
 extension Vec2f {
-    public static let one = SIMD2<Float>(repeating: 1)
+    public static let one = Vec2f(repeating: 1)
 }
 extension Vec2i {
-    public static let one = SIMD2<Int>(repeating: 1)
+    public static let one = Vec2i(repeating: 1)
 }
 extension Vec2u {
-    public static let one = SIMD2<UInt>(repeating: 1)
+    public static let one = Vec2u(repeating: 1)
 }
 
 extension Vec3d {
-    public static let one = SIMD3<Double>(repeating: 1)
+    public static let one = Vec3d(repeating: 1)
 }
 extension Vec3f {
-    public static let one = SIMD3<Float>(repeating: 1)
+    public static let one = Vec3f(repeating: 1)
 }
 extension Vec3i {
-    public static let one = SIMD3<Int>(repeating: 1)
+    public static let one = Vec3i(repeating: 1)
 }
 extension Vec3u {
-    public static let one = SIMD3<UInt>(repeating: 1)
+    public static let one = Vec3u(repeating: 1)
 }
 
 extension Vec4d {
-    public static let one = SIMD4<Double>(repeating: 1)
+    public static let one = Vec4d(repeating: 1)
 }
 extension Vec4f {
-    public static let one = SIMD4<Float>(repeating: 1)
+    public static let one = Vec4f(repeating: 1)
 }
 extension Vec4i {
-    public static let one = SIMD4<Int>(repeating: 1)
+    public static let one = Vec4i(repeating: 1)
 }
 extension Vec4u {
-    public static let one = SIMD4<UInt>(repeating: 1)
+    public static let one = Vec4u(repeating: 1)
 }
 #endif
