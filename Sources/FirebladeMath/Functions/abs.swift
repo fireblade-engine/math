bcbdee4a60a6661275b6fa89e011d2bcb9fa6968
Christian Treffs <ctreffs@gmail.com>
2019-08-27 08:58:08 +0200

    abs acos

--- Sources/FirebladeMath/Functions/abs.swift
+++ Sources/FirebladeMath/Functions/abs.swift
@@ -21,18 +21,10 @@ public func abs(_ float: Float32) -> Float32 {
 ///
 /// - Parameter double:     floating point value
 /// - Returns: If successful, returns the absolute value of arg (|arg|). The value returned is exact and does not depend on any rounding modes.
-public func abs(_ double: Float64) -> Float64 { return fabs(double) }
-
-/// Computes the principal value of the arc cosine of arg.
-///
-/// - Parameter double: floating point value
-/// - Returns: If no errors occur, the arc cosine of arg (arccos(arg)) in the range [0 ; π], is returned.
-///               If a domain error occurs, an implementation-defined value is returned (NaN where supported).
-///                If a range error occurs due to underflow, the correct result (after rounding) is returned.
-public func acos(_ double: Float64) -> Float64 {
+public func abs(_ double: Float64) -> Float64 {
     #if os(macOS) || os(iOS) || os(tvOS)
-    return Darwin.acos(double)
+    return Darwin.fabs(double)
     #elseif os(Linux)
-    return Glibc.acos(double)
+    return Glibc.fabs(double)
     #endif
 }
