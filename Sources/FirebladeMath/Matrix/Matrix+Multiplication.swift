4277fde6daa47594b7ede5bdb51f00a49813b302
Christian Treffs <ctreffs@gmail.com>
2019-09-06 11:32:22 +0200

    Multiplication

--- Sources/FirebladeMath/Matrix/Matrix+Multiplication.swift
+++ Sources/FirebladeMath/Matrix/Matrix+Multiplication.swift
@@ -11,32 +11,32 @@ import func simd.simd_mul
 
 // MARK: - Mat4x4d
 public func multiply(_ lhs: Mat4x4d, _ rhs: Mat4x4d) -> Mat4x4d {
-    #if canImport(simd)
-    return simd_mul(lhs, rhs)
+    #if USE_SIMD
+    return Mat4x4d(storage: simd_mul(lhs.storage, rhs.storage))
     #else
     fatalError("not implemented yet")
     #endif
 }
 
 public func multiply(_ lhs: Double, _ rhs: Mat4x4d) -> Mat4x4d {
-    #if canImport(simd)
-    return simd_mul(lhs, rhs)
+    #if USE_SIMD
+    return Mat4x4d(storage: simd_mul(lhs, rhs.storage))
     #else
     fatalError("not implemented yet")
     #endif
 }
 
 public func multiply(_ lhs: Vec4d, _ rhs: Mat4x4d) -> Vec4d {
-    #if canImport(simd)
-    return simd_mul(lhs, rhs)
+    #if USE_SIMD
+    return simd_mul(lhs, rhs.storage)
     #else
     fatalError("not implemented yet")
     #endif
 }
 
 public func multiply(_ lhs: Mat4x4d, _ rhs: Vec4d) -> Vec4d {
-    #if canImport(simd)
-    return simd_mul(lhs, rhs)
+    #if USE_SIMD
+    return simd_mul(lhs.storage, rhs)
     #else
     fatalError("not implemented yet")
     #endif
@@ -44,32 +44,32 @@ public func multiply(_ lhs: Mat4x4d, _ rhs: Vec4d) -> Vec4d {
 
 // MARK: - Mat4x4f
 public func multiply(_ lhs: Mat4x4f, _ rhs: Mat4x4f) -> Mat4x4f {
-    #if canImport(simd)
-    return simd_mul(lhs, rhs)
+    #if USE_SIMD
+    return Mat4x4f(storage: simd_mul(lhs.storage, rhs.storage))
     #else
     fatalError("not implemented yet")
     #endif
 }
 
 public func multiply(_ lhs: Float, _ rhs: Mat4x4f) -> Mat4x4f {
-    #if canImport(simd)
-    return simd_mul(lhs, rhs)
+    #if USE_SIMD
+    return Mat4x4f(storage: simd_mul(lhs, rhs.storage))
     #else
     fatalError("not implemented yet")
     #endif
 }
 
 public func multiply(_ lhs: Vec4f, _ rhs: Mat4x4f) -> Vec4f {
-    #if canImport(simd)
-    return simd_mul(lhs, rhs)
+    #if USE_SIMD
+    return simd_mul(lhs, rhs.storage)
     #else
     fatalError("not implemented yet")
     #endif
 }
 
 public func multiply(_ lhs: Mat4x4f, _ rhs: Vec4f) -> Vec4f {
-    #if canImport(simd)
-    return simd_mul(lhs, rhs)
+    #if USE_SIMD
+    return simd_mul(lhs.storage, rhs)
     #else
     fatalError("not implemented yet")
     #endif
