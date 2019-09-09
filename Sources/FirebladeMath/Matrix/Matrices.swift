9417fcb7fa7214715e93fbfa4455730a10928961
Christian Treffs <ctreffs@gmail.com>
2019-09-09 14:52:07 +0200

    Cleanups

--- Sources/FirebladeMath/Matrix/Matrices.swift
+++ Sources/FirebladeMath/Matrix/Matrices.swift
@@ -30,10 +30,17 @@ public typealias Mat3x3d = Matrix3x3<Storage3x3<Double>>
 
 #endif
 
+/// Get index for matrix column and row.
+/// - Parameter column: The column (zero based)
+/// - Parameter row: The row (zero based)
+/// - Parameter width: The width of a column.
 @inlinable public func index(column: Int, row: Int, width: Int) -> Int {
     return (row * width) + column
 }
 
+/// Get column and row for matrix by index.
+/// - Parameter index: The index into the matrix.
+/// - Parameter width: The width of a column.
 @inlinable public func columnRow(index: Int, width: Int) -> (column: Int, row: Int) {
     let row: Int = index % width
     let column: Int = index / width
