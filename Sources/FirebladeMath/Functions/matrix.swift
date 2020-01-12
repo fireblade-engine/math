bd21255be6e092f786b15691a4e9a362a121b56e
Christian Treffs <ctreffs@gmail.com>
2020-01-12 12:53:27 +0100

    Implement matrix from quaternion initializer non-simd

--- Sources/FirebladeMath/Functions/matrix.swift
+++ Sources/FirebladeMath/Functions/matrix.swift
@@ -14,7 +14,17 @@ public func matrix3x3(from quat: Quat4f) -> Mat3x3f {
     #if USE_SIMD
     return Mat3x3f(storage: simd_matrix3x3(quat.storage))
     #else
-    fatalError("implementation missing \(#function)")
+    let v = Vec4f(quat)
+    return Mat3x3f(
+        Vec3f(1 - 2 * (v.y * v.y + v.z * v.z),
+              2 * (v.x * v.y + v.z * v.w),
+              2 * (v.x * v.z - v.y * v.w)),
+        Vec3f(2 * (v.x * v.y - v.z * v.w),
+              1 - 2 * (v.z * v.z + v.x * v.x),
+              2 * (v.y * v.z + v.x * v.w)),
+        Vec3f(2 * (v.z * v.x + v.y * v.w),
+              2 * (v.y * v.z - v.x * v.w),
+              1 - 2 * (v.y * v.y + v.x * v.x)))
     #endif
 }
 
@@ -22,7 +32,17 @@ public func matrix3x3(from quat: Quat4d) -> Mat3x3d {
     #if USE_SIMD
     return Mat3x3d(storage: simd_matrix3x3(quat.storage))
     #else
-    fatalError("implementation missing \(#function)")
+    let v = Vec4d(quat)
+    return Mat3x3d(
+        Vec3d(1 - 2 * (v.y * v.y + v.z * v.z),
+              2 * (v.x * v.y + v.z * v.w),
+              2 * (v.x * v.z - v.y * v.w)),
+        Vec3d(2 * (v.x * v.y - v.z * v.w),
+              1 - 2 * (v.z * v.z + v.x * v.x),
+              2 * (v.y * v.z + v.x * v.w)),
+        Vec3d(2 * (v.z * v.x + v.y * v.w),
+              2 * (v.y * v.z - v.x * v.w),
+              1 - 2 * (v.y * v.y + v.x * v.x)))
     #endif
 }
 
@@ -30,7 +50,18 @@ public func matrix4x4(from quat: Quat4f) -> Mat4x4f {
     #if USE_SIMD
     return Mat4x4f(storage: simd_matrix4x4(quat.storage))
     #else
-    fatalError("implementation missing \(#function)")
+    let v = Vec4f(quat)
+    return Mat4x4f(
+        Vec4f(1 - 2 * (v.y * v.y + v.z * v.z),
+              2 * (v.x * v.y + v.z * v.w),
+              2 * (v.x * v.z - v.y * v.w), 0 ),
+        Vec4f(2 * (v.x * v.y - v.z * v.w),
+              1 - 2 * (v.z * v.z + v.x * v.x),
+              2 * (v.y * v.z + v.x * v.w), 0 ),
+        Vec4f(2 * (v.z * v.x + v.y * v.w),
+              2 * (v.y * v.z - v.x * v.w),
+              1 - 2 * (v.y * v.y + v.x * v.x), 0),
+        Vec4f( 0, 0, 0, 1 ))
     #endif
 }
 
@@ -38,6 +69,17 @@ public func matrix4x4(from quat: Quat4d) -> Mat4x4d {
     #if USE_SIMD
     return Mat4x4d(storage: simd_matrix4x4(quat.storage))
     #else
-    fatalError("implementation missing \(#function)")
+    let v = Vec4d(quat)
+    return Mat4x4d(
+        Vec4d(1 - 2 * (v.y * v.y + v.z * v.z),
+              2 * (v.x * v.y + v.z * v.w),
+              2 * (v.x * v.z - v.y * v.w), 0 ),
+        Vec4d(2 * (v.x * v.y - v.z * v.w),
+              1 - 2 * (v.z * v.z + v.x * v.x),
+              2 * (v.y * v.z + v.x * v.w), 0 ),
+        Vec4d(2 * (v.z * v.x + v.y * v.w),
+              2 * (v.y * v.z - v.x * v.w),
+              1 - 2 * (v.y * v.y + v.x * v.x), 0),
+        Vec4d( 0, 0, 0, 1 ))
     #endif
 }
