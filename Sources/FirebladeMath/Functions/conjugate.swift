a8e9815222f68073b0fa989a84060720ad189cf2
Christian Treffs <ctreffs@gmail.com>
2020-01-12 12:04:53 +0100

    Implement non-simd conjugate

--- Sources/FirebladeMath/Functions/conjugate.swift
+++ Sources/FirebladeMath/Functions/conjugate.swift
@@ -13,7 +13,7 @@ public func conjugate(_ quat: Quat4f) -> Quat4f {
     #if USE_SIMD
     return Quat4f(storage: simd.simd_conjugate(quat.storage))
     #else
-    fatalError("implementation missing \(#function)")
+    return Quat4f(Vec4f(quat) * Vec4f(-1, -1, -1, 1))
     #endif
 }
 
@@ -21,6 +21,6 @@ public func conjugate(_ quat: Quat4d) -> Quat4d {
     #if USE_SIMD
     return Quat4d(storage: simd.simd_conjugate(quat.storage))
     #else
-    fatalError("implementation missing \(#function)")
+    return Quat4d(Vec4d(quat) * Vec4d(-1, -1, -1, 1))
     #endif
 }
