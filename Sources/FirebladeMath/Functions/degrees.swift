8da12fab5cb0f4041ea34f5e638c6505cb430a24
Christian Treffs <ctreffs@gmail.com>
2020-11-01 19:25:25 +0100

    Add degree and radians conversions for Vec3f

--- Sources/FirebladeMath/Functions/degrees.swift
+++ Sources/FirebladeMath/Functions/degrees.swift
@@ -9,3 +9,10 @@ public func degrees(_ radians: Float) -> Float { radians * kRadiansToDegree32 }
 /// - Parameter radians: an angle in radians.
 /// - Returns:  the argument converted to degrees.
 public func degrees(_ radians: Double) -> Double { radians * kRadiansToDegree64 }
+
+@inline(__always)
+public func degrees(_ radians: Vec3f) -> Vec3f {
+    Vec3f(x: FirebladeMath.degrees(radians.x),
+          y: FirebladeMath.degrees(radians.y),
+          z: FirebladeMath.degrees(radians.z))
+}
