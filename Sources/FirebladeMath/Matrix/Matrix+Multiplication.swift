b10bb2b63410f7435b1f26394ada0db15717d84d
stackotter <stackotter@stackotter.dev>
2022-11-01 18:30:34 +1000

    Implement multiply for 2x2 matrices

--- Sources/FirebladeMath/Matrix/Matrix+Multiplication.swift
+++ Sources/FirebladeMath/Matrix/Matrix+Multiplication.swift
@@ -148,3 +148,105 @@ public func multiply(_ lhs: Mat4x4f, _ rhs: Vec4f) -> Vec4f {
     return SIMD4<Float>(Q, R, S, T)
     #endif
 }
+
+// MARK: - Mat2x2d
+@inlinable
+public func multiply(_ lhs: Mat2x2d, _ rhs: Mat2x2d) -> Mat2x2d {
+    #if FRB_MATH_USE_SIMD
+    return Mat2x2d(storage: simd_mul(lhs.storage, rhs.storage))
+    #else
+    return Mat2x2d(
+        Vec2d(
+            lhs[0, 0] * rhs[0, 0] + lhs[1, 0] * rhs[0, 1],
+            lhs[0, 1] * rhs[0, 0] + lhs[1, 1] * rhs[0, 1]
+        ),
+        Vec2d(
+            lhs[0, 0] * rhs[1, 0] + lhs[1, 0] * rhs[1, 1],
+            lhs[0, 1] * rhs[1, 0] + lhs[1, 1] * rhs[1, 1]
+        )
+    )
+    #endif
+}
+
+@inlinable
+public func multiply(_ lhs: Double, _ rhs: Mat2x2d) -> Mat2x2d {
+    #if FRB_MATH_USE_SIMD
+    return Mat2x2d(storage: simd_mul(lhs, rhs.storage))
+    #else
+    return Mat2x2d(lhs * rhs.storage.columns.0,
+                   lhs * rhs.storage.columns.1)
+    #endif
+}
+
+@inlinable
+public func multiply(_ lhs: Vec2d, _ rhs: Mat2x2d) -> Vec2d {
+    #if FRB_MATH_USE_SIMD
+    return simd_mul(lhs, rhs.storage)
+    #else
+    #warning("implementation missing")
+    return Vec2d.zero
+    #endif
+}
+
+@inlinable
+public func multiply(_ lhs: Mat2x2d, _ rhs: Vec2d) -> Vec2d {
+    #if FRB_MATH_USE_SIMD
+    return simd_mul(lhs.storage, rhs)
+    #else
+    let Q = lhs[0, 0] * rhs[0] + lhs[1, 0] * rhs[1]
+    let R = lhs[0, 1] * rhs[0] + lhs[1, 1] * rhs[1]
+    return SIMD2<Double>(Q, R)
+    #endif
+}
+
+// MARK: - Mat4x4f
+@inlinable
+public func multiply(_ lhs: Mat2x2f, _ rhs: Mat2x2f) -> Mat2x2f {
+    #if FRB_MATH_USE_SIMD
+    return Mat2x2f(storage: simd_mul(lhs.storage, rhs.storage))
+    #else
+    return Mat2x2f(
+        Vec2f(
+            lhs[0, 0] * rhs[0, 0] + lhs[1, 0] * rhs[0, 1],
+            lhs[0, 1] * rhs[0, 0] + lhs[1, 1] * rhs[0, 1]
+        ),
+        Vec2f(
+            lhs[0, 0] * rhs[1, 0] + lhs[1, 0] * rhs[1, 1],
+            lhs[0, 1] * rhs[1, 0] + lhs[1, 1] * rhs[1, 1]
+        )
+    )
+    #endif
+}
+
+@inlinable
+public func multiply(_ lhs: Float, _ rhs: Mat2x2f) -> Mat2x2f {
+    #if FRB_MATH_USE_SIMD
+    return Mat2x2f(storage: simd_mul(lhs, rhs.storage))
+    #else
+    return Mat2x2f(lhs * rhs.storage.column0,
+                   lhs * rhs.storage.column1,
+                   lhs * rhs.storage.column2,
+                   lhs * rhs.storage.column3)
+    #endif
+}
+
+@inlinable
+public func multiply(_ lhs: Vec2f, _ rhs: Mat2x2f) -> Vec2f {
+    #if FRB_MATH_USE_SIMD
+    return simd_mul(lhs, rhs.storage)
+    #else
+    #warning("implementation missing")
+    return Vec2f.zero
+    #endif
+}
+
+@inlinable
+public func multiply(_ lhs: Mat2x2f, _ rhs: Vec2f) -> Vec2f {
+    #if FRB_MATH_USE_SIMD
+    return simd_mul(lhs.storage, rhs)
+    #else
+    let Q = lhs[0, 0] * rhs[0] + lhs[1, 0] * rhs[1]
+    let R = lhs[0, 1] * rhs[0] + lhs[1, 1] * rhs[1]
+    return SIMD2<Float>(Q, R)
+    #endif
+}
