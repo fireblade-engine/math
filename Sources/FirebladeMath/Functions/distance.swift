4da60b4805302ac2db5b25047053d81e2374d211
Christian Treffs <ctreffs@gmail.com>
2026-08-24 09:11:14 +0200

    perf: qualify C math call in distance function

--- Sources/FirebladeMath/Functions/distance.swift
+++ Sources/FirebladeMath/Functions/distance.swift
@@ -1,3 +1,11 @@
+#if canImport(Darwin)
+import Darwin
+#elseif canImport(Glibc)
+import Glibc
+#else
+import Foundation
+#endif
+
 /// Computes the distance between the arguments.
 ///
 /// - Parameters:
@@ -7,7 +15,13 @@
 @inlinable
 public func distance(_ x: Float, _ y: Float) -> Float {
     let diff: Float = x - y
-    return FirebladeMath.abs(diff)
+    #if canImport(Darwin)
+    return Darwin.fabsf(diff)
+    #elseif canImport(Glibc)
+    return Glibc.fabsf(diff)
+    #else
+    return Foundation.fabsf(diff)
+    #endif
 }
 
 /// Computes the distance between the arguments.
@@ -19,5 +33,11 @@ public func distance(_ x: Float, _ y: Float) -> Float {
 @inlinable
 public func distance(_ x: Double, _ y: Double) -> Double {
     let diff: Double = x - y
-    return FirebladeMath.abs(diff)
+    #if canImport(Darwin)
+    return Darwin.fabs(diff)
+    #elseif canImport(Glibc)
+    return Glibc.fabs(diff)
+    #else
+    return Foundation.fabs(diff)
+    #endif
 }
