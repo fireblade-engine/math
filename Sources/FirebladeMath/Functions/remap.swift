c167be32e465fc0f929f714cd2a51991cd7e0ec3
Christian Treffs <ctreffs@gmail.com>
2026-08-24 08:42:43 +0200

    perf: specialize remap extension for Float and Double

--- Sources/FirebladeMath/Functions/remap.swift
+++ Sources/FirebladeMath/Functions/remap.swift
@@ -1,15 +1,12 @@
-/// Extension to add remapping functionality to FloatingPoint types.
-extension FloatingPoint {
+extension Float {
     /// Remaps this value from one range to another, clamping the input to the input range.
     /// - Parameters:
     ///   - rangeClamp: The input range to clamp to.
     ///   - rangeOut: The output range to map to.
     /// - Returns: The remapped value.
     @inlinable
-    public func remaped(clampingIn rangeClamp: ClosedRange<Self>,
-                        to rangeOut: ClosedRange<Self>) -> Self
-    {
-        var v = clamped(to: rangeClamp)
+    public func remaped(clampingIn rangeClamp: ClosedRange<Float>, to rangeOut: ClosedRange<Float>) -> Float {
+        var v: Float = clamped(to: rangeClamp)
         v = v.lerped(from: rangeClamp, to: rangeOut)
         return v
     }
@@ -19,9 +16,31 @@ extension FloatingPoint {
     ///   - rangeClamp: The input range to clamp to.
     ///   - rangeOut: The output range to map to.
     @inlinable
-    public mutating func remap(clampingIn rangeClamp: ClosedRange<Self>,
-                               to rangeOut: ClosedRange<Self>)
-    {
+    public mutating func remap(clampingIn rangeClamp: ClosedRange<Float>, to rangeOut: ClosedRange<Float>) {
+        clamp(to: rangeClamp)
+        lerp(from: rangeClamp, to: rangeOut)
+    }
+}
+
+extension Double {
+    /// Remaps this value from one range to another, clamping the input to the input range.
+    /// - Parameters:
+    ///   - rangeClamp: The input range to clamp to.
+    ///   - rangeOut: The output range to map to.
+    /// - Returns: The remapped value.
+    @inlinable
+    public func remaped(clampingIn rangeClamp: ClosedRange<Double>, to rangeOut: ClosedRange<Double>) -> Double {
+        var v: Double = clamped(to: rangeClamp)
+        v = v.lerped(from: rangeClamp, to: rangeOut)
+        return v
+    }
+
+    /// Remaps this value from one range to another in place, clamping the input to the input range.
+    /// - Parameters:
+    ///   - rangeClamp: The input range to clamp to.
+    ///   - rangeOut: The output range to map to.
+    @inlinable
+    public mutating func remap(clampingIn rangeClamp: ClosedRange<Double>, to rangeOut: ClosedRange<Double>) {
         clamp(to: rangeClamp)
         lerp(from: rangeClamp, to: rangeOut)
     }
